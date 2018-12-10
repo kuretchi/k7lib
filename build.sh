7277fe566f1bafc403000f79cedfa8b550651119
kuretchi <21314465+kuretchi@users.noreply.github.com>
2018-12-11 08:19:40 +0900

    Configure Travis CI

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -euv
+RUST_BACKTRACE=1
+
+cargo test
+cargo +stable doc
+cat <<EOT >./target/doc/index.html
+<!DOCTYPE html>
+<meta charset="utf-8">
+<meta http-equiv="refresh" content="0; url=spella/index.html">
+<title></title>
+EOT
