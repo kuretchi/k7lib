f627bd6cc69d4d201ca86b7dfe5f7528a25b37a0
kuretchi <21314465+kuretchi@users.noreply.github.com>
2019-09-05 23:34:05 +0900

    Use glob import for private modules

--- src/algebra/mod.rs
+++ src/algebra/mod.rs
@@ -1,14 +1,14 @@
 //! Algebraic structures.
 
-pub use self::abelian_group::AbelianGroup;
-pub use self::associative_magma::AssociativeMagma;
-pub use self::commutative_magma::CommutativeMagma;
-pub use self::group::Group;
-pub use self::invertible_magma::InvertibleMagma;
-pub use self::magma::Magma;
-pub use self::monoid::Monoid;
-pub use self::semigroup::Semigroup;
-pub use self::unital_magma::UnitalMagma;
+pub use self::abelian_group::*;
+pub use self::associative_magma::*;
+pub use self::commutative_magma::*;
+pub use self::group::*;
+pub use self::invertible_magma::*;
+pub use self::magma::*;
+pub use self::monoid::*;
+pub use self::semigroup::*;
+pub use self::unital_magma::*;
 
 mod abelian_group;
 mod associative_magma;
