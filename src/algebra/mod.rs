b8614ed1ea8879aa2728f4476de40bf770e37b2e
kuretchi <21314465+kuretchi@users.noreply.github.com>
2019-09-05 23:30:44 +0900

    Fix algebraic structures' module names

--- src/algebra/mod.rs
+++ src/algebra/mod.rs
@@ -1,21 +1,21 @@
 //! Algebraic structures.
 
 pub use self::abelian_group::AbelianGroup;
-pub use self::associative::AssociativeMagma;
-pub use self::commutative::CommutativeMagma;
+pub use self::associative_magma::AssociativeMagma;
+pub use self::commutative_magma::CommutativeMagma;
 pub use self::group::Group;
-pub use self::invertible::InvertibleMagma;
+pub use self::invertible_magma::InvertibleMagma;
 pub use self::magma::Magma;
 pub use self::monoid::Monoid;
 pub use self::semigroup::Semigroup;
-pub use self::unital::UnitalMagma;
+pub use self::unital_magma::UnitalMagma;
 
 mod abelian_group;
-mod associative;
-mod commutative;
+mod associative_magma;
+mod commutative_magma;
 mod group;
-mod invertible;
+mod invertible_magma;
 mod magma;
 mod monoid;
 mod semigroup;
-mod unital;
+mod unital_magma;
