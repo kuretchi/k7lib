34cac47471def41ec3ab01c144d8411fb3212748
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-04 16:11:19 +0000

    Deploy kuretchi/spella to github.com/kuretchi/spella.git:gh-pages

--- implementors/alloc/borrow/trait.ToOwned.js
+++ implementors/alloc/borrow/trait.ToOwned.js
@@ -1,10 +1,3 @@
 (function() {var implementors = {};
-implementors["spella"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/alloc/borrow/trait.ToOwned.html\" title=\"trait alloc::borrow::ToOwned\">ToOwned</a> for <a class=\"struct\" href=\"spella/byte/struct.ByteStr.html\" title=\"struct spella::byte::ByteStr\">ByteStr</a>",synthetic:false,types:["spella::byte::byte_str::ByteStr"]},];
-
-            if (window.register_implementors) {
-                window.register_implementors(implementors);
-            } else {
-                window.pending_implementors = implementors;
-            }
-        
-})()
+implementors["spella"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/alloc/borrow/trait.ToOwned.html\" title=\"trait alloc::borrow::ToOwned\">ToOwned</a> for <a class=\"struct\" href=\"spella/byte/struct.ByteStr.html\" title=\"struct spella::byte::ByteStr\">ByteStr</a>","synthetic":false,"types":["spella::byte::byte_str::ByteStr"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- implementors/core/borrow/trait.BorrowMut.js
+++ implementors/core/borrow/trait.BorrowMut.js
@@ -1,10 +1,3 @@
 (function() {var implementors = {};
-implementors["spella"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/borrow/trait.BorrowMut.html\" title=\"trait core::borrow::BorrowMut\">BorrowMut</a>&lt;<a class=\"struct\" href=\"spella/byte/struct.ByteStr.html\" title=\"struct spella::byte::ByteStr\">ByteStr</a>&gt; for <a class=\"struct\" href=\"spella/byte/struct.ByteString.html\" title=\"struct spella::byte::ByteString\">ByteString</a>",synthetic:false,types:["spella::byte::byte_string::ByteString"]},];
-
-            if (window.register_implementors) {
-                window.register_implementors(implementors);
-            } else {
-                window.pending_implementors = implementors;
-            }
-        
-})()
+implementors["spella"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/borrow/trait.BorrowMut.html\" title=\"trait core::borrow::BorrowMut\">BorrowMut</a>&lt;<a class=\"struct\" href=\"spella/byte/struct.ByteStr.html\" title=\"struct spella::byte::ByteStr\">ByteStr</a>&gt; for <a class=\"struct\" href=\"spella/byte/struct.ByteString.html\" title=\"struct spella::byte::ByteString\">ByteString</a>","synthetic":false,"types":["spella::byte::byte_string::ByteString"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
