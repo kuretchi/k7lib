408688ec0d2ed16e33b372977f5ed996fc9e6176
kuretchi <21314465+kuretchi@users.noreply.github.com>
2020-04-21 16:27:47 +0900

    Add algebraic systems `All` and `Any`

--- src/algebra/systems.rs
+++ src/algebra/systems.rs
@@ -1,7 +1,11 @@
 //! Algebraic systems.
 
+pub use self::all::*;
+pub use self::any::*;
 pub use self::min::*;
 pub use self::max::*;
 
+mod all;
+mod any;
 mod min;
 mod max;
---
+++ src/algebra/systems/all.rs
@@ -0,0 +1,47 @@
+use crate::algebra::structures::{AssociativeMagma, CommutativeMagma, Magma, UnitalMagma};
+
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Default, Hash, Debug)]
+pub struct All(pub bool);
+
+impl Magma for All {
+  fn op(&self, rhs: &Self) -> Self {
+    All(self.0 && rhs.0)
+  }
+}
+
+impl AssociativeMagma for All {}
+
+impl CommutativeMagma for All {}
+
+impl UnitalMagma for All {
+  fn identity() -> Self {
+    All(true)
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test() {
+    let vec = vec![true, true, false, true, false];
+    let iter = || vec.iter().copied();
+
+    assert_eq!(
+      iter().all(|x| x),
+      iter().fold(All::identity(), |acc, x| acc.op(&All(x))).0
+    );
+  }
+
+  #[test]
+  fn test_all_true() {
+    let vec = vec![true, true, true, true, true];
+    let iter = || vec.iter().copied();
+
+    assert_eq!(
+      iter().all(|x| x),
+      iter().fold(All::identity(), |acc, x| acc.op(&All(x))).0
+    );
+  }
+}
