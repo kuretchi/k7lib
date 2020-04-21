67cf3eef85768af127722cfdf4c99d29090c6e10
kuretchi <21314465+kuretchi@users.noreply.github.com>
2020-04-21 15:52:04 +0900

    Add algebraic systems `Min`/`Max`

---
+++ src/algebra/systems.rs
@@ -0,0 +1,7 @@
+//! Algebraic systems.
+
+pub use self::min::*;
+pub use self::max::*;
+
+mod min;
+mod max;
---
+++ src/algebra/systems/max.rs
@@ -0,0 +1,45 @@
+use crate::algebra::structures::{AssociativeMagma, CommutativeMagma, Magma, UnitalMagma};
+use crate::cmp::Bounded;
+
+use std::cmp;
+
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Default, Hash, Debug)]
+pub struct Max<T>(pub T);
+
+impl<T> Magma for Max<T>
+where
+  T: Clone + Ord,
+{
+  fn op(&self, rhs: &Self) -> Self {
+    Max(cmp::max(&self.0, &rhs.0).clone())
+  }
+}
+
+impl<T> AssociativeMagma for Max<T> where T: Clone + Ord {}
+
+impl<T> CommutativeMagma for Max<T> where T: Clone + Ord {}
+
+impl<T> UnitalMagma for Max<T>
+where
+  T: Clone + Ord + Bounded,
+{
+  fn identity() -> Self {
+    Max(T::min_value())
+  }
+}
+
+#[cfg(test)]
+mod test {
+  use super::*;
+
+  #[test]
+  fn test() {
+    let vec = vec![3, 1, 4, 1, 5, 9, 2, 6, 5];
+    let iter = || vec.iter().cloned();
+
+    assert_eq!(
+      iter().max().unwrap(),
+      iter().fold(Max::identity(), |acc, x| acc.op(&Max(x))).0
+    );
+  }
+}
