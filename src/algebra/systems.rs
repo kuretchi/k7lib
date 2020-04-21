af4aa9dcc4f77a71f07b30b1fcf9dfc7c252a15a
kuretchi <21314465+kuretchi@users.noreply.github.com>
2020-04-21 21:20:27 +0900

    Add algebraic systems `First` and `Last`

--- src/algebra/systems.rs
+++ src/algebra/systems.rs
@@ -2,10 +2,14 @@
 
 pub use self::all::*;
 pub use self::any::*;
+pub use self::first::*;
+pub use self::last::*;
 pub use self::max::*;
 pub use self::min::*;
 
 mod all;
 mod any;
+mod first;
+mod last;
 mod max;
 mod min;
