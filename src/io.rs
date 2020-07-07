0ca5982a4957488a707817b7fad29d97647d74cb
kuretchi <21314465+kuretchi@users.noreply.github.com>
2020-07-08 02:13:31 +0900

    Add a function `io::run`

--- src/io.rs
+++ src/io.rs
@@ -1,5 +1,7 @@
 //! Utilities related to I/O.
 
+pub use self::run::*;
 pub use self::scanner::*;
 
+mod run;
 mod scanner;
---
+++ src/io/run.rs
@@ -0,0 +1,40 @@
+use crate::io::Scanner;
+
+use std::convert;
+use std::io::{self, prelude::*, BufWriter, StdinLock};
+use std::thread;
+
+pub fn run<F>(stack_size: Option<usize>, interactive: bool, f: F) -> io::Result<()>
+where
+  F: FnOnce(&mut Scanner<StdinLock>, &mut dyn Write) -> io::Result<()>,
+  F: Send + 'static,
+{
+  let f = move || {
+    let stdin = io::stdin();
+    let reader = stdin.lock();
+    let mut scanner = Scanner::new(reader);
+    let stdout = io::stdout();
+    let writer = stdout.lock();
+    macro_rules! with {
+      ($wrapper:expr) => {{
+        let mut writer = $wrapper(writer);
+        f(&mut scanner, &mut writer)?;
+        writer.flush()
+      }};
+    }
+    if cfg!(debug_assertions) || interactive {
+      with!(convert::identity)
+    } else {
+      with!(BufWriter::new)
+    }
+  };
+
+  match stack_size {
+    Some(stack_size) => thread::Builder::new()
+      .stack_size(stack_size)
+      .spawn(f)?
+      .join()
+      .unwrap(),
+    None => f(),
+  }
+}
