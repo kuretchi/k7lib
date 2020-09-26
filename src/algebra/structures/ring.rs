32c4515be0829266940260e99bb0974406e9c539
kuretchi <21314465+kuretchi@users.noreply.github.com>
2020-09-26 18:28:56 +0900

    Add docs

--- src/algebra/structures/ring.rs
+++ src/algebra/structures/ring.rs
@@ -2,6 +2,9 @@ use super::Semiring;
 use crate::num::primitive::SignedInt as PrimSint;
 
 /// A ring.
+///
+/// # Laws
+/// * ∀`x` (`x.add(&x.neg())` = `x.neg().add(&x)` = `Self::zero()`)
 pub trait Ring: Semiring {
   /// Returns an additive inverse.
   fn neg(&self) -> Self;
--- src/algebra/structures/semiring.rs
+++ src/algebra/structures/semiring.rs
@@ -1,6 +1,19 @@
 use crate::num::primitive::Int as PrimInt;
 
 /// A semiring.
+///
+/// # Laws
+/// * It forms a commutative monoid under addition.
+///   * Associativity: ∀`x` ∀`y` ∀`z` (`x.add(&y).add(&z)` = `x.add(&y.add(&z))`)
+///   * Identity: ∀`x` (`x.add(&Self::zero())` = `Self::zero().add(&x)` = `x`)
+///   * Commutativity: ∀`x` ∀`y` (`x.add(&y)` = `y.add(&x)`)
+/// * It forms a monoid under multiplication.
+///   * Associativity: ∀`x` ∀`y` ∀`z` (`x.mul(&y).mul(&z)` = `x.mul(&y.mul(&z))`)
+///   * Identity: ∀`x` (`x.mul(&Self::one())` = `Self::one().mul(&x)` = `x`)
+/// * Distributivity:
+///   * Left: ∀`x` ∀`y` ∀`z` (`x.mul(&y.add(&z))` = `x.mul(&y).add(&x.mul(&z))`)
+///   * Right: ∀`x` ∀`y` ∀`z` (`x.add(&y).mul(&z)` = `x.mul(&z).add(&y.mul(&z))`)
+/// * ∀`x` (`x.mul(&Self::zero())` = `Self::zero().mul(&x)` = `Self::zero()`)
 pub trait Semiring: Clone {
   /// Performs addition.
   fn add(&self, rhs: &Self) -> Self;
@@ -32,6 +45,7 @@ impl Semiring for () {
   }
 }
 
+/// Usual addition and multiplication over integers.
 impl<Int> Semiring for Int
 where
   Int: PrimInt,
