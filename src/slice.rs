10ec31e60dd09def550b63ef867264b46461799e
kuretchi <21314465+kuretchi@users.noreply.github.com>
2020-11-11 02:12:48 +0900

    Move `next_permutation` from `slice` to `string`

--- src/slice.rs
+++ src/slice.rs
@@ -1,5 +1,1 @@
 //! Extensions for slices.
-
-pub use self::next_permutation::*;
-
-mod next_permutation;
--- src/string.rs
+++ src/string.rs
@@ -1,1 +1,5 @@
 //! Algorithms and data structures for processing strings.
+
+pub use self::next_permutation::*;
+
+mod next_permutation;
