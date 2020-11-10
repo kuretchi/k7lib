01d54e65aeaf78ab2a3bfdd9e1c7b58131a48261
kuretchi <21314465+kuretchi@users.noreply.github.com>
2020-11-10 23:54:51 +0900

    Rename variables

--- src/slice/next_permutation.rs
+++ src/slice/next_permutation.rs
@@ -1,38 +1,38 @@
 use std::cmp::Ordering::{self, *};
 
-pub fn next_permutation<T>(xs: &mut [T]) -> bool
+pub fn next_permutation<T>(s: &mut [T]) -> bool
 where
   T: Ord,
 {
-  next_permutation_by(xs, T::cmp)
+  next_permutation_by(s, T::cmp)
 }
 
-pub fn next_permutation_by<T, F>(xs: &mut [T], mut cmp: F) -> bool
+pub fn next_permutation_by<T, F>(s: &mut [T], mut cmp: F) -> bool
 where
   F: FnMut(&T, &T) -> Ordering,
 {
-  let i = match xs.windows(2).rposition(|w| cmp(&w[0], &w[1]) == Less) {
+  let i = match s.windows(2).rposition(|w| cmp(&w[0], &w[1]) == Less) {
     Some(i) => i,
     None => return false,
   };
-  let j = xs.iter().rposition(|x| cmp(x, &xs[i]) == Greater).unwrap();
+  let j = s.iter().rposition(|c| cmp(c, &s[i]) == Greater).unwrap();
   //     i        j
   // [3, 2, 5, 4, 4, 2, 1]
   //        ^^^^^^^^^^^^^ sorted in descending order
-  xs.swap(i, j);
+  s.swap(i, j);
   //     i
   // [3, 4, 5, 4, 2, 2, 1]
   //        ^^^^^^^^^^^^^ sorted in descending order
-  xs[i + 1..].reverse();
+  s[i + 1..].reverse();
   // [3, 4, 1, 2, 2, 4, 5]
   //        ^^^^^^^^^^^^^ sorted in ascending order
   true
 }
 
-pub fn next_permutation_by_key<T, K, F>(xs: &mut [T], mut f: F) -> bool
+pub fn next_permutation_by_key<T, K, F>(s: &mut [T], mut f: F) -> bool
 where
   F: FnMut(&T) -> K,
   K: Ord,
 {
-  next_permutation_by(xs, |l, r| f(l).cmp(&f(r)))
+  next_permutation_by(s, |l, r| f(l).cmp(&f(r)))
 }
