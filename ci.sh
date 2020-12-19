a40e7c5bbc96def9652035e262732f550621a29d
kuretchi <21314465+kuretchi@users.noreply.github.com>
2020-12-19 17:31:10 +0900

    Fix variables for quickcheck

--- ci.sh
+++ ci.sh
@@ -2,7 +2,10 @@
 set -euv
 
 export RUST_BACKTRACE=1
-export QUICKCHECK_GENERATOR_SIZE=500
+export QUICKCHECK_TESTS=2000
+# Allow to generate all values of `u8`
+# See https://github.com/BurntSushi/quickcheck/blob/0.9.2/src/arbitrary.rs#L760-L762
+export QUICKCHECK_GENERATOR_SIZE=255
 
 # Run tests
 cargo test
